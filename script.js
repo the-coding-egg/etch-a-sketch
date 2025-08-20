1ee889aa8c00e517c9668ca6f1358e7367e7c8f7
Dalton McMullen <thecodingegg@gmail.com>
2025-08-19 20:14:43 -0700

    Created skeleton
    Created skeleton of file structure. Next plan is to build the boxes to
    show on the screen.

---
+++ script.js
@@ -0,0 +1,8 @@
+const grid = document.createElement("div");
+document.container.appendChild(grid);
+
+grid.style.width = "100px";
+grid.style.height = "100px";
+grid.style.border = "1px solid black";
+
+
