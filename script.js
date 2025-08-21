b2d80c3148ede0b799b68cb030e75921032d776b
Dalton McMullen <dltn761@gmail.com>
2025-08-20 21:30:32 -0700

    Built rows
    Need to create functionality for the rows to be filled with columns
    (i.e., create a grid).

--- script.js
+++ script.js
@@ -1,21 +1,36 @@
-const grid = document.createElement("div");
 const container = document.querySelector(".container");
+const row = document.createElement("div");
+const column = document.createElement("div");
 
 container.style.display = "flex";
-grid.style.width = "100px";
-grid.style.height = "100px";
-grid.style.border = "2px solid black";
 
-const requestedGrid = parseInt(prompt("Please input how big you want the grid"));
-if (requestedGrid <= 100 && requestedGrid > 0) {
-  for (let i = 1; i <= requestedGrid; i++) {
-    const fullGrid = grid.cloneNode(true);
-    container.appendChild(fullGrid);
+column.style.flex = "1";
+column.style.height = "25px";
+column.style.border = "2px solid black";
+
+row.style.flex = "1";
+row.style.height = "25px";
+row.style.border = "2px solid black";
+
+const btn = document.createElement("button");
+btn.textContent = "Grid Size";
+document.body.appendChild(btn);
+
+
+btn.addEventListener("click", () => {
+  const requestedGrid = parseInt(prompt("Please input how big you want the grid"));
+  if (requestedGrid <= 100 && requestedGrid > 0) {
+    for (let i = 1; i <= requestedGrid; i++) {
+      const columns = column.cloneNode(true);
+      container.appendChild(columns);
+      const rows = row.cloneNode(true);
+      container.appendChild(rows);
+    }
+  } else {
+    console.log("Number needs to be 0-100. Try again");
   }
-} else {
-  console.log("Number needs to be 0-100. Try again");
+});
 
-}
 
 
 
