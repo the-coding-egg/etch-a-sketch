71a3cf3e15645a9745b66c6fe410890f868d9368
Dalton McMullen <dltn761@gmail.com>
2025-08-20 19:56:03 -0700

    Created input for how many boxes
    Now able to take the input for how many boxes and display them on the
    screen. Added flex to make them horizontal. Next steps are to make them
    a grid.

--- script.js
+++ script.js
@@ -1,8 +1,23 @@
 const grid = document.createElement("div");
-document.container.appendChild(grid);
+const container = document.querySelector(".container");
 
+container.style.display = "flex";
 grid.style.width = "100px";
 grid.style.height = "100px";
-grid.style.border = "1px solid black";
+grid.style.border = "2px solid black";
+
+const requestedGrid = parseInt(prompt("Please input how big you want the grid"));
+if (requestedGrid <= 100 && requestedGrid > 0) {
+  for (let i = 1; i <= requestedGrid; i++) {
+    const fullGrid = grid.cloneNode(true);
+    container.appendChild(fullGrid);
+  }
+} else {
+  console.log("Number needs to be 0-100. Try again");
+
+}
+
+
+
 
 
