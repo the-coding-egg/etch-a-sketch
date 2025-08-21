3b6bb759a39e1eb833ff491f5a611f10a1bf6f23
Dalton McMullen <thecodingegg@gmail.com>
2025-08-21 16:54:34 -0700

    Added rows & columns with a centered button
    Centered the button and added the functionality for it to create both
    rows and columns. Now need to clean up the style of the boxes so they
    cover the same amount of screen everytime. Next is also the hover
    functionality.

--- script.js
+++ script.js
@@ -1,30 +1,32 @@
 const container = document.querySelector(".container");
-const row = document.createElement("div");
-const column = document.createElement("div");
 
 container.style.display = "flex";
 
-column.style.flex = "1";
-column.style.height = "25px";
-column.style.border = "2px solid black";
-
-row.style.flex = "1";
-row.style.height = "25px";
-row.style.border = "2px solid black";
-
 const btn = document.createElement("button");
 btn.textContent = "Grid Size";
-document.body.appendChild(btn);
-
+document.body.style.textAlign = "center";
+document.body.prepend(btn);
+btn.style.margin = "20px";
 
 btn.addEventListener("click", () => {
   const requestedGrid = parseInt(prompt("Please input how big you want the grid"));
   if (requestedGrid <= 100 && requestedGrid > 0) {
     for (let i = 1; i <= requestedGrid; i++) {
-      const columns = column.cloneNode(true);
-      container.appendChild(columns);
-      const rows = row.cloneNode(true);
-      container.appendChild(rows);
+      const row = document.createElement("div");
+      row.style.flex = "1";
+      row.style.height = "25px";
+      row.style.border = "2px solid black";
+      row.classList.add("row");
+      container.appendChild(row);
+
+      for (let j = 1; j <= requestedGrid; j++) {
+        const column = document.createElement("div");
+        column.style.flex = "1";
+        column.style.height = "25px";
+        column.style.border = "2px solid black";
+        column.classList.add("column");
+        row.appendChild(column);
+      }
     }
   } else {
     console.log("Number needs to be 0-100. Try again");
