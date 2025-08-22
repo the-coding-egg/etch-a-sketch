59c91b45002185657e3f689cfcbf5ee91ff0a0b2
Dalton McMullen <thecodingegg@gmail.com>
2025-08-22 10:27:00 -0700

    Created ability to maintain grid within a set pixel size
    Added the ability to keep the grid in a certain size. Going to branch
    off and see if I can create functions to help this out

--- script.js
+++ script.js
@@ -1,29 +1,43 @@
+//create the container element
 const container = document.querySelector(".container");
 
+//style the container
 container.style.display = "flex";
+container.style.flex = "wrap";
+container.style.width = "600px";
+container.style.alignItems = "center";
+container.style.justifyContent = "center";
 
+//ensure the container is the same width every time
+const containerWidth = container.clientWidth;
+
+//create the button
 const btn = document.createElement("button");
 btn.textContent = "Grid Size";
 document.body.style.textAlign = "center";
 document.body.prepend(btn);
 btn.style.margin = "20px";
+btn.style.backgroundColor = "#46383A";
+btn.style.color = "white";
+btn.style.borderRadius = "5px";
 
+//add functionality of button
 btn.addEventListener("click", () => {
+  container.innerHTML = "";
   const requestedGrid = parseInt(prompt("Please input how big you want the grid"));
   if (requestedGrid <= 100 && requestedGrid > 0) {
     for (let i = 1; i <= requestedGrid; i++) {
       const row = document.createElement("div");
       row.style.flex = "1";
-      row.style.height = "25px";
-      row.style.border = "2px solid black";
+      row.style.border = "1px solid black";
       row.classList.add("row");
       container.appendChild(row);
 
       for (let j = 1; j <= requestedGrid; j++) {
         const column = document.createElement("div");
         column.style.flex = "1";
-        column.style.height = "25px";
-        column.style.border = "2px solid black";
+        column.style.border = "1px solid black";
+        column.style.height = "100px";
         column.classList.add("column");
         row.appendChild(column);
       }
