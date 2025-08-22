e6b764d1bdc5ba7762846c41735e137a84d07493
Dalton McMullen <thecodingegg@gmail.com>
2025-08-22 11:17:47 -0700

    Corrected the box to display correctly
    Finally was able to maneuver the box into fitting into a grid and
    placing it in the center. Next steps are to hover over the boxes and
    change the color.

--- script.js
+++ script.js
@@ -1,16 +1,6 @@
 //create the container element
 const container = document.querySelector(".container");
 
-//style the container
-container.style.display = "flex";
-container.style.flex = "wrap";
-container.style.width = "600px";
-container.style.alignItems = "center";
-container.style.justifyContent = "center";
-
-//ensure the container is the same width every time
-const containerWidth = container.clientWidth;
-
 //create the button
 const btn = document.createElement("button");
 btn.textContent = "Grid Size";
@@ -21,34 +11,30 @@ btn.style.backgroundColor = "#46383A";
 btn.style.color = "white";
 btn.style.borderRadius = "5px";
 
-//add functionality of button
 btn.addEventListener("click", () => {
-  container.innerHTML = "";
-  const requestedGrid = parseInt(prompt("Please input how big you want the grid"));
-  if (requestedGrid <= 100 && requestedGrid > 0) {
-    for (let i = 1; i <= requestedGrid; i++) {
-      const row = document.createElement("div");
-      row.style.flex = "1";
-      row.style.border = "1px solid black";
-      row.classList.add("row");
-      container.appendChild(row);
-
-      for (let j = 1; j <= requestedGrid; j++) {
-        const column = document.createElement("div");
-        column.style.flex = "1";
-        column.style.border = "1px solid black";
-        column.style.height = "100px";
-        column.classList.add("column");
-        row.appendChild(column);
-      }
-    }
+  const size = parseInt(prompt("Enter grid size."));
+  if (size <= 100 && size > 0) {
+    createGrid(size);
   } else {
-    console.log("Number needs to be 0-100. Try again");
+    alert("Entry needs to be between 0-100");
   }
 });
 
+function createGrid(size) {
+  container.innerHTML = "";
 
+  for (let i = 0; i < size * size; i++) {
+    const grid = document.createElement("div");
+    grid.className = "grid";
 
+    const cellSize = 100 / size;
+    grid.style.width = `${cellSize}%`;
+    grid.style.aspectRatio = "1 / 1";
+    grid.style.border = "1px solid black";
+    grid.style.boxSizing = "border-box";
 
+    container.appendChild(grid);
+  }
+}
 
-
+createGrid(16);
